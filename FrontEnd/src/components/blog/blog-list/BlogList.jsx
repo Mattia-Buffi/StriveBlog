6aba1d006fff777b557d3f02c060c3384f105a3c
Mattia Buffello <buffello@me.com>
2024-04-21 21:05:41 +0200

    first coomit

---
+++ FrontEnd/src/components/blog/blog-list/BlogList.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Col, Row } from "react-bootstrap";
+import posts from "../../../data/posts.json";
+import BlogItem from "../blog-item/BlogItem";
+
+//fare la fetch al datbase per recuperare i post
+
+const BlogList = props => {
+  return (
+    <Row>
+      {posts.map((post, i) => (
+        <Col
+          key={`item-${i}`}
+          md={4}
+          style={{
+            marginBottom: 50,
+          }}
+        >
+          <BlogItem key={post.title} {...post} />
+        </Col>
+      ))}
+    </Row>
+  );
+};
+
+export default BlogList;
